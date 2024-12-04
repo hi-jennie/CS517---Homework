f3e6ec97034923abeb47ad8091cb9e513c7f11ab
hi-jennie <jennie.wang2024@gmail.com>
2024-12-04 14:33:19 +0800

    finishing Applying for a Job function

--- hw1/badger-mart/jobs.js
+++ hw1/badger-mart/jobs.js
@@ -2,4 +2,23 @@ function submitApplication(e) {
     e.preventDefault(); // You can ignore this; prevents the default form submission!
 
     // TODO: Alert the user of the job that they applied for!
+    const allJobs = document.getElementsByName('job');
+
+    // wrong version: forEach is not breakable
+    // allJobs.forEach(job => {
+    //     console.log(1);
+    //     if (job.checked) {
+    //         alert(`You applied for ${job.value}`);
+    //         break;
+    //     }
+    // });
+
+    for(let job of allJobs) {
+        if (job.checked) {
+            alert(`Thank you for applying to be a ${job.value}`);
+            return;
+        }
+    }
+    alert('Please select a job!');
+    
 }
