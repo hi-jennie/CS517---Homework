e0e07ca7b3099ff5857acc131c6c6ac0e6e62b53
hi-jennie <jennie.wang2024@gmail.com>
2024-12-16 16:16:17 +0800

    add some style to the display page

--- hw5/src/components/nav/pages/BadgerBudSummary.jsx
+++ hw5/src/components/nav/pages/BadgerBudSummary.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import {Button, Card} from "react-bootstrap"
 export default function BadgerBudSummary(props) {
-    return <Card >
+    return <Card className="d-flex flex-column align-items-center ">
         <img 
             src={`https://raw.githubusercontent.com/CS571-F24/hw5-api-static-content/main/cats/${props.imgIds[0]}`} 
             style={{
@@ -11,8 +11,11 @@ export default function BadgerBudSummary(props) {
               }}
             alt={`A picture of ${props.name}`}
         />
-        <p>{props.name}</p>
-        <Button>show more</Button>
-        <Button variant="success">save</Button>
+        <h2>{props.name}</h2>
+        <div style={{display: "flex", justifyContent:"center",padding:"10px", width:" 100%", gap: "10px", backgroundColor: "#f5f5f5"}}>
+            <Button>show more</Button>
+            <Button variant="secondary">💗save</Button>
+        </div>
+
     </Card>
 }
