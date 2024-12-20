5196bd52655929a3a3fc95651851705f0f84be83
hi-jennie <jennie.wang2024@gmail.com>
2024-12-20 14:51:43 +0800

    finish Logout

--- hw6/src/components/auth/BadgerLogout.jsx
+++ hw6/src/components/auth/BadgerLogout.jsx
@@ -1,17 +1,19 @@
 
-import React, { useEffect } from 'react';
-
+import React, { useEffect,useContext } from 'react';
+import BadgerLoginStatusContext from "../contexts/BadgerLoginStatusContext";
 export default function BadgerLogout() {
-
+    const [loginStatus, setLoginStatus] = useContext(BadgerLoginStatusContext)
     useEffect(() => {
         fetch('https://cs571.org/rest/f24/hw6/logout', {
             method: 'POST',
             headers: {
-                "X-CS571-ID": CS571.getBadgerId()
+                "X-CS571-ID": "bid_6fdf3569a0589bf7a2ad2e4065b73b940a57be11eaf482cbc41b9c16c9fc7e75"
             },
             credentials: "include"
         }).then(res => res.json()).then(json => {
-            // Maybe you need to do something here?
+            setLoginStatus(false);
+            sessionStorage.setItem("loginStatus", JSON.stringify(false));
+            alert(json.msg)
         })
     }, []);
 
