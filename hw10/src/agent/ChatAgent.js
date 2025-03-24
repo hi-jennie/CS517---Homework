2fff5e55f6288bd157e9d46fb6e21285694eb6d3
hi-jennie <jennie.wang2024@gmail.com>
2025-03-24 11:09:37 +0800

    finished Get Items function

--- hw10/src/agent/ChatAgent.js
+++ hw10/src/agent/ChatAgent.js
@@ -7,15 +7,6 @@ const createChatAgent = () => {
     let availableItems = [];
 
     const handleInitialize = async () => {
-        const response = await fetch("https://cs571.org/rest/f24/hw10/items", {
-            method: "GET",
-            headers: {
-                "X-CS571-ID": "bid_6fdf3569a0589bf7a2ad2e4065b73b940a57be11eaf482cbc41b9c16c9fc7e75"
-            }
-        })
-        const data = await response.json();
-        availableItems = data;
-        console.log(availableItems);
         return "Welcome to BadgerMart Voice! Type your question, or ask for help if you're lost!";
     }
 
@@ -35,12 +26,27 @@ const createChatAgent = () => {
             const intentName = data.intents[0].name;
             if (intentName === "ask_help") {
                 return await askHelp()
+            } else if (intentName === "get_items") {
+                return await getItems()
             }
         }
     }
 
     async function askHelp() {
         return "In BadgerMart Voice, you can get the list of items, the price of an item, add or remove an item from you cart"
+    }
+
+    async function getItems() {
+        const response = await fetch("https://cs571.org/rest/f24/hw10/items", {
+            method: "GET",
+            headers: {
+                "X-CS571-ID": "bid_6fdf3569a0589bf7a2ad2e4065b73b940a57be11eaf482cbc41b9c16c9fc7e75"
+            }
+        })
+        const data = await response.json();
+        availableItems = data;
+        console.log(availableItems);
+        return "We have " + availableItems.map(item => item.name).join(", ");
     }
 
     return {
