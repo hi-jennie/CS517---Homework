213662687964da9b3bc2e40695ad63f14f81265f
hi-jennie <jennie.wang2024@gmail.com>
2025-03-24 11:50:32 +0800

    finished Get Price for Item

--- hw10/src/agent/ChatAgent.js
+++ hw10/src/agent/ChatAgent.js
@@ -7,11 +7,19 @@ const createChatAgent = () => {
     let availableItems = [];
 
     const handleInitialize = async () => {
+        const response = await fetch("https://cs571.org/rest/f24/hw10/items", {
+            method: "GET",
+            headers: {
+                "X-CS571-ID": "bid_6fdf3569a0589bf7a2ad2e4065b73b940a57be11eaf482cbc41b9c16c9fc7e75"
+            }
+        })
+        const data = await response.json();
+        availableItems = data;
+        console.log(availableItems);
         return "Welcome to BadgerMart Voice! Type your question, or ask for help if you're lost!";
     }
 
     const handleReceive = async (prompt) => {
-        // TODO: Replace this with your code to handle a user's message!
         let res = await fetch("https://api.wit.ai/message?q=" + encodeURIComponent(prompt), {
             headers: {
                 "Authorization": "Bearer " + CS571_WITAI_ACCESS_TOKEN
@@ -28,6 +36,11 @@ const createChatAgent = () => {
                 return await askHelp()
             } else if (intentName === "get_items") {
                 return await getItems()
+            } else if (intentName === "ask_price") {
+                // how to get the item in an object: using key value pair: entities is an object, using the key "type:type" to get the value
+                // the value of "type:type" is an array, so we use [0] to get the first element
+                // It's kind of confusing !!!
+                return await getItemPrice(data.entities["type:type"][0]["value"])
             }
         }
     }
@@ -37,18 +50,20 @@ const createChatAgent = () => {
     }
 
     async function getItems() {
-        const response = await fetch("https://cs571.org/rest/f24/hw10/items", {
-            method: "GET",
-            headers: {
-                "X-CS571-ID": "bid_6fdf3569a0589bf7a2ad2e4065b73b940a57be11eaf482cbc41b9c16c9fc7e75"
-            }
-        })
-        const data = await response.json();
-        availableItems = data;
-        console.log(availableItems);
         return "We have " + availableItems.map(item => item.name).join(", ");
     }
 
+    async function getItemPrice(itemName) {
+        // filter function returns an array
+        console.log(availableItems)
+        const targetItem = availableItems.filter(item => item.name === itemName);
+        console.log(targetItem)
+        if (targetItem.length === 0) {
+            return "Sorry, the item is not in stock";
+        } else {
+            return `${itemName} cost $${targetItem[0].price} each`;
+        }
+    }
     return {
         handleInitialize,
         handleReceive
