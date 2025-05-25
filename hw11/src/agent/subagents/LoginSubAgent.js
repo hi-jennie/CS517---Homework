678c364e7b587a84815058c0ee1d2f2f3ee798cd
hi-jennie <jennie.wang2024@gmail.com>
2025-05-25 22:04:57 +0800

    finished login

--- hw11/src/agent/subagents/LoginSubAgent.js
+++ hw11/src/agent/subagents/LoginSubAgent.js
@@ -1,13 +1,56 @@
+import { isLoggedIn, ofRandom, getLoggedInUsername } from "../Util";
 const createLoginSubAgent = (end) => {
 
     let stage;
+    let username;
+    let password;
 
     const handleInitialize = async (promptData) => {
-        return end("I should try to login...")
+        // 在if (isLogin) 不能直接使用isLoggedIn() 应为他是一个promise
+        const isLogin = await isLoggedIn();
+        if (isLogin) {
+            console.log(isLogin)
+            return end("you need to be logged out before logging in")
+        } else {
+            stage = "FOLLOWUP_USERNAME"
+            return ofRandom(["what's your username ?", "tell me your username"])
+        }
     }
 
     const handleReceive = async (prompt) => {
-        
+        switch (stage) {
+            case "FOLLOWUP_USERNAME": return await handleFollowupUsername(prompt);
+            case "FOLLOWUP_PASSWORD": return await handleFollowupPassword(prompt);
+        }
+    }
+
+    const handleFollowupUsername = async (prompt) => {
+        username = prompt;
+        stage = "FOLLOWUP_PASSWORD";
+        return ofRandom(["Great, what's your password?", " How about your password"]);
+    }
+
+    const handleFollowupPassword = async (prompt) => {
+        password = prompt
+        console.log(password)
+        stage = undefined;
+        const res = await fetch("https://cs571.org/rest/f24/hw11/login", {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                "X-CS571-ID": "bid_6fdf3569a0589bf7a2ad2e4065b73b940a57be11eaf482cbc41b9c16c9fc7e75",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username: username,
+                pin: password
+            })
+        })
+        if (res.status === 200) {
+            return end("Login successful!");
+        } else {
+            return end("Login failed, please check your credentials and try again.");
+        }
     }
 
     return {
