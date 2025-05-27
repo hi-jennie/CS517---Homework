5efcdd8df4f1a2b6b4b82a093a3beea14f592222
hi-jennie <jennie.wang2024@gmail.com>
2025-05-27 18:26:07 +0800

    using prepared sql query in case of malicious comments

--- ice-fullstack/starter/backend/src/index.js
+++ ice-fullstack/starter/backend/src/index.js
@@ -8,6 +8,7 @@ import { open } from 'sqlite'
 
 import { applyRateLimiting, applyLooseCORSPolicy, applyBodyParsing, applyLogging, applyErrorCatching } from './api-middleware.js'
 
+// let comments = []
 const app = express();
 const port = 53706;
 
@@ -23,7 +24,7 @@ const db = await open({
 
 await db.exec("CREATE TABLE IF NOT EXISTS BadgerComment(id INTEGER PRIMARY KEY UNIQUE, comment TEXT NOT NULL, created TIMESTAMP NOT NULL);")
 
-let comments = [];
+
 applyRateLimiting(app);
 applyLooseCORSPolicy(app);
 applyBodyParsing(app);
@@ -36,34 +37,39 @@ app.get('/api/hello-world', (req, res) => {
 })
 
 app.get('/api/comments', async (req, res) => {
-
+    const comments = await db.all(GET_POST_SQL)
     res.status(200).send(comments)
 })
 
 app.post('/api/comments', async (req, res) => {
     const comment = req.body.comment;
-    let commentId = comments.length;
-    let commentDt = new Date();
-    let commentObj = {
-        id: commentId,
-        comment: comment,
-        created: commentDt
-    }
-    comments.push(commentObj)
+    // let commentId = comments.length;
+    // let commentDt = new Date();
+    // let commentObj = {
+    //     id: commentId,
+    //     comment: comment,
+    //     created: commentDt
+    // }
+    // comments.push(commentObj)
 
+    // there is a risk using exec cause it will execute all the query, if the user post a comment like "DROP TABLE BadgerComment". 
+    // It will cause disastrous consequences.
+
+    // await db.exec(`INSERT INTO BadgerComment(comment, created) VALUES ("${comment}", "${new Date()}")`)
+    const id = await db.get(INSERT_POST_SQL, comment, new Date())
     res.status(200).send({
         msg: "I should create a comment.",
-        id: commentId
+        id: id
     })
 })
 
 app.delete('/api/comments', async (req, res) => {
     const commentId = req.query.id;
-    comments = comments.filter(c => c.id != commentId);
+    // comments = comments.filter(c => c.id != commentId);
+    await db.run(DELETE_POST_SQL, commentId)
 
     res.status(200).send({
-        msg: "I should delete a comment.",
-        len: comments.length
+        msg: "I for sure deleted a comment.",
     })
 });
 
